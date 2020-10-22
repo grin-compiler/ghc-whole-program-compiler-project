43f5e1aaf0bbf7de86f59d80e85b685761efb6b6
Andor Penzes <andor.penzes@gmail.com>
2020-10-22 21:33:48 +0200

    TagToEnum primops.

--- external-stg-interpreter/lib/Stg/Interpreter/PrimOp/TagToEnum.hs
+++ external-stg-interpreter/lib/Stg/Interpreter/PrimOp/TagToEnum.hs
@@ -1,27 +1,28 @@
-{-# LANGUAGE RecordWildCards, LambdaCase, OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards, LambdaCase, OverloadedStrings, PatternSynonyms #-}
 module Stg.Interpreter.PrimOp.TagToEnum where
 
+import Data.List (findIndex)
 import Stg.Syntax
 import Stg.Interpreter.Base
+
+pattern IntV :: Integer -> Atom
+pattern IntV i = Literal (LitNumber LitNumInt i)
 
 evalPrimOp :: PrimOpEval -> Name -> [Atom] -> Type -> Maybe TyCon -> M [Atom]
 evalPrimOp fallback op args t tc = case (op, args) of
 
+  -- dataToTag# :: a -> Int#  -- Zero-indexed; the first constructor has tag zero
+  ("dataToTag#", [addr@HeapPtr{}]) -> do
+    (Con dataCon []) <- readHeapCon addr
+    case findIndex (\d -> dcId d == dcId dataCon) (tcDataCons (dcTyCon dataCon)) of
+      Nothing -> stgErrorM $ "Data constructor is not found for " ++ show (dcId dataCon)
+      Just i -> pure [IntV $ fromIntegral i]
+
+  -- tagToEnum# :: Int# -> a
+  ("tagToEnum", [IntV i]) -> do
+    Just tyc <- pure tc
+    let dc = tcDataCons tyc !! (fromIntegral i)
+    loc <- allocAndStore (Con dc [])
+    pure [HeapPtr loc]
+
   _ -> fallback op args t tc
-
-{-
-------------------------------------------------------------------------
-section "Tag to enum stuff"
-        {Convert back and forth between values of enumerated types
-        and small integers.}
-------------------------------------------------------------------------
-
-primop  DataToTagOp "dataToTag#" GenPrimOp
-   a -> Int#  -- Zero-indexed; the first constructor has tag zero
-   with
-   strictness = { \ _arity -> mkClosedStrictSig [evalDmd] topDiv }
-   -- See Note [dataToTag# magic] in GHC.Core.Op.ConstantFold
-
-primop  TagToEnumOp "tagToEnum#" GenPrimOp
-   Int# -> a
--}
