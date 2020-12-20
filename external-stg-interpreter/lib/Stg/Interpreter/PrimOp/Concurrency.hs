1f981f59056f7732cffb2c3734ba6e234157ef1d
Csaba Hruska <csaba.hruska@gmail.com>
2020-12-20 13:19:40 +0100

    update thread status primop

--- external-stg-interpreter/lib/Stg/Interpreter/PrimOp/Concurrency.hs
+++ external-stg-interpreter/lib/Stg/Interpreter/PrimOp/Concurrency.hs
@@ -14,13 +14,9 @@ pattern IntV i = IntAtom i
 evalPrimOp :: PrimOpEval -> Name -> [Atom] -> Type -> Maybe TyCon -> M [Atom]
 evalPrimOp fallback op args t tc = case (op, args) of
 
-  ("noDuplicate#", [_s]) -> pure [] --  State# s -> State# s
-  -------------------------
-
   -- fork# :: a -> State# RealWorld -> (# State# RealWorld, ThreadId# #)
   ( "fork#", [ioAction, _s]) -> do
-    currentTId <- gets ssCurrentThreadId
-    currentTS <- lookupThreadState currentTId
+    currentTS <- getCurrentThreadState
 
     (newTId, newTS) <- createThread
     updateThreadState newTId $ newTS
@@ -39,8 +35,7 @@ evalPrimOp fallback op args t tc = case (op, args) of
 
   -- forkOn# :: Int# -> a -> State# RealWorld -> (# State# RealWorld, ThreadId# #)
   ( "forkOn#", [IntV capabilityNo, ioAction, _s]) -> do
-    currentTId <- gets ssCurrentThreadId
-    currentTS <- lookupThreadState currentTId
+    currentTS <- getCurrentThreadState
 
     (newTId, newTS) <- createThread
     updateThreadState newTId $ newTS
@@ -85,12 +80,13 @@ evalPrimOp fallback op args t tc = case (op, args) of
 
   -- isCurrentThreadBound# :: State# RealWorld -> (# State# RealWorld, Int# #)
   ( "isCurrentThreadBound#", [_s]) -> do
-    tid <- gets ssCurrentThreadId
-    ThreadState{..} <- lookupThreadState tid
+    ThreadState{..} <- getCurrentThreadState
     pure [IntV $ if tsBound then 1 else 0]
 
   -- noDuplicate# :: State# s -> State# s
-  -- TODO
+  ( "noDuplicate#", [_s]) -> do
+    -- NOTE: the stg interpreter is not concurrent, so this is a no-op
+    pure []
 
   -- threadStatus# :: ThreadId# -> State# RealWorld -> (# State# RealWorld, Int#, Int#, Int# #)
   ( "threadStatus#", [ThreadId tid, _s]) -> do
@@ -102,91 +98,26 @@ evalPrimOp fallback op args t tc = case (op, args) of
           ThreadFinished  -> 16
           ThreadDied      -> 17
           ThreadBlocked r -> case r of
-            BlockedOnMVar         -> 1
+            BlockedOnMVar{}       -> 1
+            BlockedOnMVarRead{}   -> 14
             BlockedOnBlackHole    -> 2
-            BlockedOnException    -> 12
             BlockedOnSTM          -> 6
             BlockedOnForeignCall  -> 10
-            BlockedOnOther        -> 3 -- TODO: do we need this? BlockedOnRead 3 ; BlockedOnWrite 4 ; BlockedOnDelay 5
+            BlockedOnRead{}       -> 3
+            BlockedOnWrite{}      -> 4
+            BlockedOnDelay{}      -> 5
+            BlockedOnThrowAsyncException -> 12
 
     pure [IntV statusCode, IntV tsCapability, IntV $ if tsLocked then 1 else 0]
 
   _ -> fallback op args t tc
 
 {-
-------------------------------------------------------------------------
-section "Concurrency primitives"
-------------------------------------------------------------------------
-
-primtype State# s
-        { {\tt State\#} is the primitive, unlifted type of states.  It has
-        one type parameter, thus {\tt State\# RealWorld}, or {\tt State\# s},
-        where s is a type variable. The only purpose of the type parameter
-        is to keep different state threads separate.  It is represented by
-        nothing at all. }
-
-primtype RealWorld
-        { {\tt RealWorld} is deeply magical.  It is {\it primitive}, but it is not
-        {\it unlifted} (hence {\tt ptrArg}).  We never manipulate values of type
-        {\tt RealWorld}; it's only used in the type system, to parameterise {\tt State\#}. }
-
-primtype ThreadId#
-        {(In a non-concurrent implementation, this can be a singleton
-        type, whose (unique) value is returned by {\tt myThreadId\#}.  The
-        other operations can be omitted.)}
-
-primop  ForkOp "fork#" GenPrimOp
-   a -> State# RealWorld -> (# State# RealWorld, ThreadId# #)
-   with
-   has_side_effects = True
-   out_of_line      = True
-
-primop  ForkOnOp "forkOn#" GenPrimOp
-   Int# -> a -> State# RealWorld -> (# State# RealWorld, ThreadId# #)
-   with
-   has_side_effects = True
-   out_of_line      = True
-
 primop  KillThreadOp "killThread#"  GenPrimOp
    ThreadId# -> a -> State# RealWorld -> State# RealWorld
    with
    has_side_effects = True
    out_of_line      = True
-
-primop  YieldOp "yield#" GenPrimOp
-   State# RealWorld -> State# RealWorld
-   with
-   has_side_effects = True
-   out_of_line      = True
-
-primop  MyThreadIdOp "myThreadId#" GenPrimOp
-   State# RealWorld -> (# State# RealWorld, ThreadId# #)
-   with
-   has_side_effects = True
-
-primop LabelThreadOp "labelThread#" GenPrimOp
-   ThreadId# -> Addr# -> State# RealWorld -> State# RealWorld
-   with
-   has_side_effects = True
-   out_of_line      = True
-
-primop  IsCurrentThreadBoundOp "isCurrentThreadBound#" GenPrimOp
-   State# RealWorld -> (# State# RealWorld, Int# #)
-   with
-   out_of_line = True
-   has_side_effects = True
-
-primop  NoDuplicateOp "noDuplicate#" GenPrimOp
-   State# s -> State# s
-   with
-   out_of_line = True
-   has_side_effects = True
-
-primop  ThreadStatusOp "threadStatus#" GenPrimOp
-   ThreadId# -> State# RealWorld -> (# State# RealWorld, Int#, Int#, Int# #)
-   with
-   out_of_line = True
-   has_side_effects = True
 -}
 
 {-
