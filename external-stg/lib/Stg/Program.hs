87e1b87a065ee0af36a9c496d0da115e5214b53b
Csaba Hruska <csaba.hruska@gmail.com>
2021-02-20 12:42:34 +0100

    improve .stglib lookup ; handle noHsMain .stgapp option

--- external-stg/lib/Stg/Program.hs
+++ external-stg/lib/Stg/Program.hs
@@ -30,35 +30,58 @@ moduleToModpak modpakExt moduleName = replaceEq '.' '/' moduleName ++ modpakExt
 parseSection :: [String] -> String -> [String]
 parseSection content n = map (read . tail) . takeWhile (isPrefixOf "-") . tail . dropWhile (not . isPrefixOf n) $ content
 
+parsePathSection :: FilePath -> [String] -> String -> [String]
+parsePathSection root content n = map (\p -> if isAbsolute p then p else root </> p) $ parseSection content n
+
 data StgAppInfo
   = StgAppInfo
   { appIncludePaths   :: [String]
   , appLibPaths       :: [String]
   , appLdOptions      :: [String]
   , appCLikeObjFiles  :: [String]
+  , appNoHsMain       :: Bool
   }
   deriving (Eq, Ord, Show)
 
 getAppInfo :: FilePath -> IO StgAppInfo
 getAppInfo ghcStgAppFname = do
   content <- lines <$> readFile ghcStgAppFname
-  let libPaths  = parseSection content "pkg_lib_paths:"
-      incPaths  = parseSection content "pkg_include_paths:"
-      [root]    = parseSection content "root:"
-      cObjFiles = parseSection content "extra_ld_inputs:"
-      ldFlags   = parseSection content "other_flags:"
-      extraLibs = parseSection content "extra_libs:"
-      pkgLibs   = parseSection content "package_hs_libs:"
-      cLibs     = filter (isPrefixOf "-lC") pkgLibs
-  archives <- Set.fromList . map takeFileName . concat <$> mapM (find always (extension ==? ".a")) libPaths
-  let cbits   = [o ++ ".o_cbits" | o <- pkgLibs, Set.member ("lib" ++ drop 2 o ++ ".o_cbits.a") archives]
-      stubs   = [o ++ ".o_stubs" | o <- pkgLibs, Set.member ("lib" ++ drop 2 o ++ ".o_stubs.a") archives]
-      ldOpts  = concat [stubs, cbits, cLibs, ldFlags, extraLibs]
+  let [root]      = parseSection content "root:"
+      [osuf]      = parseSection content "o_suffix:"
+      libPaths    = parsePathSection root content "pkg_lib_paths:"
+      incPaths    = parsePathSection root content "pkg_include_paths:"
+      extra_input = parsePathSection root content "extra_ld_inputs:"
+      ldFlags     = parseSection content "other_flags:"
+      extraLibs   = parseSection content "extra_libs:"
+      pkgLibs     = parseSection content "package_hs_libs:"
+      nonHsLibs   = filter (not . isPrefixOf "-lHS") pkgLibs
+      [noHsMain]  = parseSection content "no_hs_main:"
+
+  -- HINT: if there is no corresponding modpak then it is a C like obj
+  cObjFiles <- filterM (\n -> not <$> doesFileExist (n ++ "_modpak")) extra_input
+
+  archiveList <- concat <$> mapM (find always (extension ==? ".a")) libPaths
+  let archiveMap        = Map.fromList [(takeFileName n, takeDirectory n) | n <- archiveList]
+      libArName n kind  = "lib" ++ drop 2 n ++ "." ++ osuf ++ kind ++".a"
+      libLdOpt n kind   = n ++ "." ++ osuf ++ kind
+  let cbits   = [ (libLdOpt o "_cbits", arPath)
+                | o <- pkgLibs
+                , let arName = libArName o "_cbits"
+                , arPath <- maybeToList $ Map.lookup arName archiveMap
+                ]
+      stubs   = [ (libLdOpt o "_stubs", arPath)
+                | o <- pkgLibs
+                , let arName = libArName o "_stubs"
+                , arPath <- maybeToList $ Map.lookup arName archiveMap
+                ]
+      ldOpts  = concat [map fst stubs, map fst cbits, nonHsLibs, ldFlags, extraLibs]
+
   pure StgAppInfo
     { appIncludePaths   = incPaths
-    , appLibPaths       = libPaths
+    , appLibPaths       = libPaths ++ map snd stubs ++ map snd cbits
     , appLdOptions      = ldOpts
-    , appCLikeObjFiles  = [root </> o | o <- cObjFiles]
+    , appCLikeObjFiles  = cObjFiles
+    , appNoHsMain       = "True" == noHsMain
     }
 
 data StgModuleInfo
@@ -70,10 +93,11 @@ data StgModuleInfo
   deriving (Eq, Ord, Show)
 
 getLibModuleMapping :: FilePath -> IO [StgModuleInfo]
-getLibModuleMapping stglibPath = do
-  let packageName = takeFileName $ dropTrailingPathSeparator stglibPath
-  fname <- head <$> find always (extension ==? ".o_stglib") stglibPath
-  content <- lines <$> readFile fname
+getLibModuleMapping stglibFname = do
+  -- HINT: the folder containing the .stglib is named after the package name
+  let packageName = takeFileName $ dropTrailingPathSeparator $ dropFileName stglibFname
+      stglibPath  = dropFileName stglibFname
+  content <- lines <$> readFile stglibFname
   let modules   = parseSection content "modules:"
       modpakExt = takeExtension . head $ parseSection content "modpaks:"
   pure
@@ -89,21 +113,26 @@ getAppModuleMapping :: FilePath -> IO [StgModuleInfo]
 getAppModuleMapping ghcStgAppFname = do
   let packageName = "exe:" ++ takeBaseName ghcStgAppFname -- TODO: save package to .stgapp and .stglib
   content <- lines <$> readFile ghcStgAppFname
-  let paths     = parseSection content "pkg_lib_paths:"
-      [root]    = parseSection content "root:"
-      o_files   = parseSection content "o_files:"
-  libModpaks <- mapM getLibModuleMapping paths
+  let [root]      = parseSection content "root:"
+      [osuf]      = parseSection content "o_suffix:"
+      libPaths    = parsePathSection root content "pkg_lib_paths:"
+      o_files     = parsePathSection root content "o_files:"
+      extra_input = parsePathSection root content "extra_ld_inputs:"
+      pkgLibs     = parseSection content "package_hs_libs:"
+      pkgHSLibs   = [drop 4 n | n <- pkgLibs, isPrefixOf "-lHS" n]
+  stglibs <- mapM (findStglibForHSLib osuf libPaths) pkgHSLibs
+  libModpaks <- mapM getLibModuleMapping stglibs
 
+  extraAppModpaks <- filterM doesFileExist $ map (++ "_modpak") extra_input
+  let appModpaks  = map (++ "_modpak") o_files
   -- load app module names from stgbins
-  appModules <- forM o_files $ \o -> do
-    let modpak = root </> o ++ "_modpak"
+  appModules <- forM (appModpaks ++ extraAppModpaks) $ \modpak -> do
     (_phase, _unitId, moduleName, _srcPath) <- readModpakL modpak modpakStgbinPath decodeStgbinModuleName
     pure StgModuleInfo
           { modModuleName   = BS8.unpack $ getModuleName moduleName
           , modModpakPath   = modpak
           , modPackageName  = packageName
           }
-
   pure $ appModules ++ concat libModpaks
 
 getAppModpaks :: FilePath -> IO [FilePath]
@@ -157,3 +186,21 @@ getGhcStgAppModules ghcstgappPath = do
   appModpaks <- collectProgramModules' False (map modModpakPath modinfoList) "main" "Main" GHCSymbols.liveSymbols
   forM appModpaks $ \modpakName -> do
     readModpakL modpakName modpakStgbinPath decodeStgbin
+
+-- .stglib utility
+findStglibForHSLib :: String -> [FilePath] -> String -> IO FilePath
+findStglibForHSLib osuf hsLibPaths libName = do
+  -- HINT: e.g. libHSghc-boot-8.11.0.20201112-ghc8.11.0.20201112.dyn_o_stglib
+  let stglibName
+        | isPrefixOf "rts-1.0" libName  -- HACK!!
+        = "libHSrts-1.0.o_stglib"
+
+        | otherwise
+        = "libHS" ++ libName ++ "." ++ osuf ++ "_stglib"
+
+  filesFound <- forM hsLibPaths $ \path -> do
+    find always (fileName ==? stglibName) path
+  case concat filesFound of
+    [stglibPath]  -> pure stglibPath
+    []            -> error $ "stglib not found: " ++ stglibName
+    l             -> error $ "multiple stglib found: " ++ stglibName ++ "\n" ++ show l
