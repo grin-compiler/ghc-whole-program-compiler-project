ea84926ddaefa76dff98a8fe6b7960f507392550
Csaba Hruska <csaba.hruska@gmail.com>
2021-03-08 19:27:23 +0100

    add simple object lifetime report

--- external-stg-interpreter/lib/Stg/Interpreter/GC.hs
+++ external-stg-interpreter/lib/Stg/Interpreter/GC.hs
@@ -10,6 +10,7 @@ import qualified Data.IntSet as IntSet
 
 import Control.Concurrent
 
+import Stg.Syntax
 import Stg.Interpreter.Base
 import Stg.Interpreter.GC.LiveDataAnalysis
 
@@ -20,6 +21,7 @@ checkGC extraGCRoots = do
   lastGCAddr <- gets ssLastGCAddr
   gcIsRunning <- gets ssGCIsRunning
   when (not gcIsRunning && nextAddr - lastGCAddr > 300000) $ do
+    lifetimeAnalysis
     modify' $ \s -> s {ssLastGCAddr = nextAddr, ssGCIsRunning = True}
     runGC extraGCRoots
     {-
@@ -40,8 +42,8 @@ analysisLoop :: MVar ([Atom], StgState) -> MVar DeadData -> IO ()
 analysisLoop gcIn gcOut = do
   (extraGCRoots, stgState) <- takeMVar gcIn
   deadData <- runLiveDataAnalysis extraGCRoots stgState
+  reportRemovedData stgState deadData
   putMVar gcOut deadData
-  --reportRemovedData stgState deadData
   analysisLoop gcIn gcOut
 
 init :: IO (ThreadId, MVar ([Atom], StgState), MVar DeadData)
@@ -59,7 +61,7 @@ tryPrune = do
     Just deadData -> do
       stgState <- get
       -- remove dead data from stg state
-      --liftIO $ putStrLn " * done GC"
+      liftIO $ putStrLn " * done GC"
       put $ (pruneStgState stgState deadData) {ssGCIsRunning = False}
 
 runGCAsync :: [Atom] -> M ()
@@ -67,7 +69,7 @@ runGCAsync extraGCRoots = do
   stgState <- get
   PrintableMVar gcIn <- gets ssGCInput
   liftIO $ do
-    --putStrLn " * start GC"
+    putStrLn " * start GC"
     putMVar gcIn (extraGCRoots, stgState)
 
 -- synchronous GC
@@ -98,6 +100,7 @@ pruneStgState stgState@StgState{..} DeadData{..} = stgState
   , ssMutableArrayArrays  = IntMap.withoutKeys ssMutableArrayArrays  deadMutableArrayArrays
   , ssMutableByteArrays   = IntMap.withoutKeys ssMutableByteArrays   deadMutableByteArrays
   , ssStableNameMap       = Map.filter (`IntSet.notMember` deadStableNames) ssStableNameMap
+--  , ssThreads             = IntMap.filter (isThreadLive . tsStatus)  ssThreads
   }
 
 reportRemovedData :: StgState -> DeadData -> IO ()
@@ -112,8 +115,8 @@ reportRemovedData StgState{..} DeadData{..} = do
       reportI = report_ IntMap.size
       reportM = report_ Map.size
 
-  putStrLn "freed after GC:"
 
+  putStrLn "freed after GC:"
   reportI "ssHeap               " ssHeap deadHeap
   reportI "ssWeakPointers       " ssWeakPointers deadWeakPointers
   reportI "ssMVars              " ssMVars deadMVars
@@ -126,3 +129,56 @@ reportRemovedData StgState{..} DeadData{..} = do
   reportI "ssMutableArrayArrays " ssMutableArrayArrays deadMutableArrayArrays
   reportI "ssMutableByteArrays  " ssMutableByteArrays deadMutableByteArrays
   reportM "ssStableNameMap      " ssStableNameMap deadStableNames
+
+  let threads = IntMap.elems ssThreads
+  printf "live threads: %-6d  all threads: %d\n" (length $ [ts | ts <- threads, isThreadLive (tsStatus ts)]) (length threads)
+
+lifetimeAnalysis :: M ()
+lifetimeAnalysis = do
+  heap <- gets ssHeap
+  heapStart <- gets ssHeapStartAddress
+  let age = 600000 -- heapStart - 1
+  let (_, dynHeap) = IntMap.split age heap
+      n = 100
+      some = take n $ IntMap.toList dynHeap
+  liftIO $ do
+    printf "the first %d younger than %d (age) non-static heap objects:\n" n age
+    forM_ some $ \(i, o) -> printf "%-8d %3s  %s\n" i (ppLNE o) (debugPrintHeapObject o)
+
+debugPrintHeapObject :: HeapObject -> String
+debugPrintHeapObject  = \case
+  Con{..}           -> "Con: " ++ show (dcUniqueName hoCon) ++ " " ++ show hoConArgs
+  Closure{..}       -> "Clo: " ++ show hoName ++ " args: " ++ show hoCloArgs ++ " missing: " ++ show hoCloMissing
+  BlackHole o       -> "BlackHole - " ++ debugPrintHeapObject o
+  ApStack{}         -> "ApStack"
+  RaiseException ex -> "RaiseException: " ++ show ex
+
+ppLNE :: HeapObject -> String
+ppLNE = \case
+  Con{..} | hoIsLNE     -> "LNE"
+  Closure{..} | hoIsLNE -> "LNE"
+  _ -> ""
+
+{-
+  TODO:
+    - add stack namespace
+    - add extra root namespace
+    - add only the stack and extra to gcroot
+    - add stack and extra to reference relation
+  with the Retain relation we could see the retainer origin
+
+  show:
+    - origin    addr + closure name + live/dead
+    - retainer  addr + closure name
+
+  new debug commands:
+    - stop      ; pause execution and listen to commands
+    - retainer  ; show retainer
+    - origin    ; show value origin
+    - info      ; show heap object + 'retainer' + 'origin'
+    - track-retainer  ; track back values to GCRoots and print retainer objects their origins ; transitive closure of 'info'
+    - track-origin    ; track back value origin to the oldest living object (show 'info' after each step)
+    - report-state    ; report stack + current thread + next heap/array/etc. addresses
+    - gc        ; run gc
+    - lifetime age count   ; call lifetimeAnalysis with the age threshold and item count
+-}
