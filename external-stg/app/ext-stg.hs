4d81083c96838a4a51037e3a1dd0f18a3d07d4a3
Csaba Hruska <csaba.hruska@gmail.com>
2021-05-13 18:33:04 +0200

    pretty print ext stg either from .modpak or .stgbin

--- external-stg/app/ext-stg.hs
+++ external-stg/app/ext-stg.hs
@@ -2,7 +2,7 @@
 
 import Control.Monad
 import Data.Maybe
-import Data.List (sortBy)
+import Data.List (sortBy, isSuffixOf)
 import Data.Monoid
 import Data.Ord
 
@@ -10,6 +10,7 @@ import Options.Applicative
 import Text.PrettyPrint.ANSI.Leijen hiding ((<$>), (<>))
 import qualified Text.PrettyPrint.ANSI.Leijen as PP
 import qualified Data.ByteString.Char8 as BS8
+import qualified Data.ByteString.Lazy as BSL
 
 import Stg.Syntax
 import Stg.Pretty
@@ -24,14 +25,17 @@ modes = subparser
     mode name f opts = command name (info (helper <*> f) opts)
 
     modpakFile :: Parser FilePath
-    modpakFile = argument str (metavar "MODPAK_FILENAME" <> help "pretty prints external stg from .modpak file")
+    modpakFile = argument str (metavar "MODPAK_OR_STGBIN" <> help "pretty prints external stg from .modpak or .stgbin file")
 
     showMode :: Parser (IO ())
     showMode =
         run <$> modpakFile
       where
         run fname = do
-            dump <- Stg.IO.readModpakL fname modpakStgbinPath decodeStgbin
+            dump <- case () of
+              _ | isSuffixOf "modpak" fname -> Stg.IO.readModpakL fname modpakStgbinPath decodeStgbin
+              _ | isSuffixOf "stgbin" fname -> decodeStgbin <$> BSL.readFile fname
+              _ -> fail "unknown file format"
             print $ pprModule dump
 
 main :: IO ()
