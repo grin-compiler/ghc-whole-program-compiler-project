d783d9ffb3746390397071198e35a5e445c97270
Csaba Hruska <csaba.hruska@gmail.com>
2021-10-02 15:56:25 +0200

    update fake rts and the precompiled foreign code lib

--- external-stg-interpreter/data/cbits.so-script/c-src/fake_rts.c
+++ external-stg-interpreter/data/cbits.so-script/c-src/fake_rts.c
@@ -1,3 +1,6 @@
+#include <stddef.h>
+#include <stdint.h>
+
 unsigned int n_capabilities = 1;
 int rts_isDynamic(void) {
   return 1;
@@ -28,3 +31,19 @@ typedef struct _RTS_FLAGS {
 RTS_FLAGS RtsFlags;
 
 int keepCAFs;
+
+void performGC(void) {
+}
+
+void performMajorGC(void) {
+}
+
+int getRTSStatsEnabled( void ) {
+    return 0;//RtsFlags.GcFlags.giveStats != NO_GC_STATS;
+}
+
+uint32_t enabled_capabilities = 1;
+
+int *stable_ptr_table = NULL;
+
+void base_GHCziTopHandler_runIO_closure(){}
