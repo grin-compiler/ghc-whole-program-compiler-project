1b3244c25b978adbd5fefebcbf6063e411526bb6
Csaba Hruska <csaba.hruska@gmail.com>
2021-12-06 16:23:33 +0100

    add dep export feature to fullpak cli tool

--- external-stg/app/fullpak.hs
+++ external-stg/app/fullpak.hs
@@ -1,10 +1,14 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, RecordWildCards, OverloadedStrings #-}
 
 import Control.Monad
 import Data.Maybe
 import Data.List (isSuffixOf)
 import Data.Monoid
 import Data.Ord
+import Data.Semigroup ((<>))
+import qualified Data.ByteString.Char8 as BS8
+import System.FilePath
+import System.IO
 
 import Options.Applicative
 
@@ -12,9 +16,17 @@ import Stg.Syntax
 import Stg.Analysis.Closure
 import Stg.Program
 
+loadModules :: FilePath -> IO [Module]
+loadModules fname = case () of
+  _ | isSuffixOf "fullpak" fname    -> Stg.Program.getFullpakModules fname
+  _ | isSuffixOf "ghc_stgapp" fname -> Stg.Program.getGhcStgAppModules fname
+  _ | isSuffixOf "json" fname       -> Stg.Program.getJSONModules fname
+  _ -> fail "unknown file format"
+
 modes :: Parser (IO ())
 modes = subparser
     (  mode "stats" statsMode (progDesc "print fullpak statistics")
+    <> mode "deps"  depsMode  (progDesc "write out module dependency facts")
     )
   where
     mode :: String -> Parser a -> InfoMod a -> Mod CommandFields a
@@ -28,12 +40,27 @@ modes = subparser
         run <$> fullpakFile
       where
         run fname = do
-            moduleList <- case () of
-              _ | isSuffixOf "fullpak" fname    -> Stg.Program.getFullpakModules fname
-              _ | isSuffixOf "ghc_stgapp" fname -> Stg.Program.getGhcStgAppModules fname
-              _ | isSuffixOf "json" fname       -> Stg.Program.getJSONModules fname
-              _ -> fail "unknown file format"
+            moduleList <- loadModules fname
             putStrLn $ "total number of closures: " ++ show (length $ concatMap getAllClosures moduleList)
+
+    depsMode :: Parser (IO ())
+    depsMode =
+        run <$> fullpakFile
+      where
+        run fname = do
+            moduleList <- loadModules fname
+            withFile (fname -<.> ".deps.tsv") WriteMode $ \h -> do
+              hPutStrLn h "Source\tTarget"
+              forM_ moduleList $ \Module{..} -> do
+                let modName = getUnitId moduleUnitId <> "_" <> getModuleName moduleName
+                    deps    = [ modName <> "\t" <> depName
+                              | (uid, mods) <- moduleDependency
+                              , mod <- mods
+                              , let depName = getUnitId uid <> "_" <> getModuleName mod
+                              , depName /= modName
+                              ]
+                BS8.hPutStr h $ BS8.unlines deps
+            pure ()
 
 main :: IO ()
 main = join $ execParser $ info (helper <*> modes) mempty
