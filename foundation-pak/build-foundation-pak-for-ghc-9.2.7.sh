51e806b9f072a57496f524aca6448dcd9afd7b3d
Csaba Hruska <csaba.hruska@gmail.com>
2023-06-06 17:45:48 +0200

    macos support

--- foundation-pak/build-foundation-pak-for-ghc-9.2.7.sh
+++ foundation-pak/build-foundation-pak-for-ghc-9.2.7.sh
@@ -25,6 +25,7 @@ case "${unameOut}" in
       sudo tlmgr install framed
       sudo tlmgr install needspace
       sudo tlmgr install tabulary
+      sudo tlmgr install varwidth
       sudo tlmgr install adjustbox
       sudo tlmgr install tcolorbox
       sudo tlmgr install collectbox
