f73867d8c015cddfafdb9577cc6302b64d8976f7
Csaba Hruska <csaba.hruska@gmail.com>
2024-11-14 15:29:07 +0100

    update wired in table

--- external-stg-interpreter/lib/Stg/Interpreter/Rts.hs
+++ external-stg-interpreter/lib/Stg/Interpreter/Rts.hs
@@ -80,9 +80,13 @@ initRtsSupport progName progArgs mods = do
                 , bName == binderName
                 ]
 
+  promptM_ $ do
+    forM_ mods $ \m@Module{..} -> do
+      liftIO $ print (moduleUnitId, moduleName)
+
   forM_ wiredInClosures $ \(u, m, n, setter) -> do
     case Map.lookup (u, m, n) closureMap of
-        Nothing -> error $ "missing wired in closure: " ++ show (u, m, n)
+        Nothing -> liftIO $ putStrLn $ "missing wired in closure: " ++ show (u, m, n)-- ++ "\n" ++ unlines (map show $ Map.keys closureMap)
         Just b  -> do
           cl <- lookupEnv mempty b
           modify' $ \s@StgState{..} -> s {ssRtsSupport = setter ssRtsSupport cl}
@@ -111,25 +115,25 @@ wiredInCons =
   -- unit-id,     module,       type con,     data con
   [ ("ghc-prim",  "GHC.Types",  "Char",       "C#",         \s dc -> s {rtsCharCon      = dc})
   , ("ghc-prim",  "GHC.Types",  "Int",        "I#",         \s dc -> s {rtsIntCon       = dc})
-  , ("base",      "GHC.Int",    "Int8",       "I8#",        \s dc -> s {rtsInt8Con      = dc})
-  , ("base",      "GHC.Int",    "Int16",      "I16#",       \s dc -> s {rtsInt16Con     = dc})
-  , ("base",      "GHC.Int",    "Int32",      "I32#",       \s dc -> s {rtsInt32Con     = dc})
-  , ("base",      "GHC.Int",    "Int64",      "I64#",       \s dc -> s {rtsInt64Con     = dc})
   , ("ghc-prim",  "GHC.Types",  "Word",       "W#",         \s dc -> s {rtsWordCon      = dc})
-  , ("base",      "GHC.Word",   "Word8",      "W8#",        \s dc -> s {rtsWord8Con     = dc})
-  , ("base",      "GHC.Word",   "Word16",     "W16#",       \s dc -> s {rtsWord16Con    = dc})
-  , ("base",      "GHC.Word",   "Word32",     "W32#",       \s dc -> s {rtsWord32Con    = dc})
-  , ("base",      "GHC.Word",   "Word64",     "W64#",       \s dc -> s {rtsWord64Con    = dc})
-  , ("base",      "GHC.Ptr",    "Ptr",        "Ptr",        \s dc -> s {rtsPtrCon       = dc})
-  , ("base",      "GHC.Ptr",    "FunPtr",     "FunPtr",     \s dc -> s {rtsFunPtrCon    = dc})
   , ("ghc-prim",  "GHC.Types",  "Float",      "F#",         \s dc -> s {rtsFloatCon     = dc})
   , ("ghc-prim",  "GHC.Types",  "Double",     "D#",         \s dc -> s {rtsDoubleCon    = dc})
-  , ("base",      "GHC.Stable", "StablePtr",  "StablePtr",  \s dc -> s {rtsStablePtrCon = dc})
   , ("ghc-prim",  "GHC.Types",  "Bool",       "True",       \s dc -> s {rtsTrueCon      = dc})
   , ("ghc-prim",  "GHC.Types",  "Bool",       "False",      \s dc -> s {rtsFalseCon     = dc})
+  , ("ghc-internal",      "GHC.Internal.Int",    "Int8",       "I8#",        \s dc -> s {rtsInt8Con      = dc})
+  , ("ghc-internal",      "GHC.Internal.Int",    "Int16",      "I16#",       \s dc -> s {rtsInt16Con     = dc})
+  , ("ghc-internal",      "GHC.Internal.Int",    "Int32",      "I32#",       \s dc -> s {rtsInt32Con     = dc})
+  , ("ghc-internal",      "GHC.Internal.Int",    "Int64",      "I64#",       \s dc -> s {rtsInt64Con     = dc})
+  , ("ghc-internal",      "GHC.Internal.Word",   "Word8",      "W8#",        \s dc -> s {rtsWord8Con     = dc})
+  , ("ghc-internal",      "GHC.Internal.Word",   "Word16",     "W16#",       \s dc -> s {rtsWord16Con    = dc})
+  , ("ghc-internal",      "GHC.Internal.Word",   "Word32",     "W32#",       \s dc -> s {rtsWord32Con    = dc})
+  , ("ghc-internal",      "GHC.Internal.Word",   "Word64",     "W64#",       \s dc -> s {rtsWord64Con    = dc})
+  , ("ghc-internal",      "GHC.Internal.Ptr",    "Ptr",        "Ptr",        \s dc -> s {rtsPtrCon       = dc})
+  , ("ghc-internal",      "GHC.Internal.Ptr",    "FunPtr",     "FunPtr",     \s dc -> s {rtsFunPtrCon    = dc})
+  , ("ghc-internal",      "GHC.Internal.Stable", "StablePtr",  "StablePtr",  \s dc -> s {rtsStablePtrCon = dc})
 
   -- validation for extStgRtsSupportModule
-  , ("ghc-prim",  "GHC.Tuple",  "(,)",        "(,)",        \s _dc -> s)
+  , ("ghc-prim",  "GHC.Tuple",  "Tuple2",        "(,)",        \s _dc -> s)
   ]
 {-
          "-Wl,-u,ghczmprim_GHCziTuple_Z0T_closure"
@@ -142,19 +146,19 @@ wiredInCons =
 wiredInClosures :: [(Name, Name, Name, Rts -> Atom -> Rts)]
 wiredInClosures =
   -- unit-id,     module,                   binder,                         closure setter
-  [ ("base",      "GHC.TopHandler",         "runIO",                        \s cl -> s {rtsTopHandlerRunIO            = cl})
-  , ("base",      "GHC.TopHandler",         "runNonIO",                     \s cl -> s {rtsTopHandlerRunNonIO         = cl})
-  , ("base",      "GHC.TopHandler",         "flushStdHandles",              \s cl -> s {rtsTopHandlerFlushStdHandles  = cl})
-  , ("base",      "GHC.Pack",               "unpackCString",                \s cl -> s {rtsUnpackCString              = cl})
-  , ("base",      "GHC.Exception.Type",     "divZeroException",             \s cl -> s {rtsDivZeroException           = cl})
-  , ("base",      "GHC.Exception.Type",     "underflowException",           \s cl -> s {rtsUnderflowException         = cl})
-  , ("base",      "GHC.Exception.Type",     "overflowException",            \s cl -> s {rtsOverflowException          = cl})
+  [ ("ghc-internal",      "GHC.Internal.TopHandler",         "runIO",                        \s cl -> s {rtsTopHandlerRunIO            = cl})
+  , ("ghc-internal",      "GHC.Internal.TopHandler",         "runNonIO",                     \s cl -> s {rtsTopHandlerRunNonIO         = cl})
+  , ("ghc-internal",      "GHC.Internal.TopHandler",         "flushStdHandles",              \s cl -> s {rtsTopHandlerFlushStdHandles  = cl})
+  , ("ghc-internal",      "GHC.Internal.Pack",               "unpackCString",                \s cl -> s {rtsUnpackCString              = cl})
+  , ("ghc-internal",      "GHC.Internal.Exception.Type",     "divZeroException",             \s cl -> s {rtsDivZeroException           = cl})
+  , ("ghc-internal",      "GHC.Internal.Exception.Type",     "underflowException",           \s cl -> s {rtsUnderflowException         = cl})
+  , ("ghc-internal",      "GHC.Internal.Exception.Type",     "overflowException",            \s cl -> s {rtsOverflowException          = cl})
   , (":ext-stg",  ":ExtStg.RTS.Support",    "applyFun1Arg",                 \s cl -> s {rtsApplyFun1Arg               = cl})
   , (":ext-stg",  ":ExtStg.RTS.Support",    "tuple2Proj0",                  \s cl -> s {rtsTuple2Proj0                = cl})
-  , ("base",      "Control.Exception.Base", "nestedAtomically",             \s cl -> s {rtsNestedAtomically           = cl})
-  , ("base",      "Control.Exception.Base", "nonTermination",               \s cl -> s {rtsNonTermination             = cl})
-  , ("base",      "GHC.IO.Exception",       "blockedIndefinitelyOnMVar",    \s cl -> s {rtsBlockedIndefinitelyOnMVar  = cl})
-  , ("base",      "GHC.IO.Exception",       "blockedIndefinitelyOnSTM",     \s cl -> s {rtsBlockedIndefinitelyOnSTM   = cl})
+  , ("ghc-internal",      "GHC.Internal.Control.Exception.Base", "nestedAtomically",             \s cl -> s {rtsNestedAtomically           = cl})
+  , ("ghc-internal",      "GHC.Internal.Control.Exception.Base", "nonTermination",               \s cl -> s {rtsNonTermination             = cl})
+  , ("ghc-internal",      "GHC.Internal.IO.Exception",       "blockedIndefinitelyOnMVar",    \s cl -> s {rtsBlockedIndefinitelyOnMVar  = cl})
+  , ("ghc-internal",      "GHC.Internal.IO.Exception",       "blockedIndefinitelyOnSTM",     \s cl -> s {rtsBlockedIndefinitelyOnSTM   = cl})
   ]
 
 {-
